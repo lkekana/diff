6dc29de458c59f156e1f27297a54c517f7503bf3
Lesedi Kekana <theoriginalles@gmail.com>
2026-04-17 15:51:02 +0200

    install tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 
 	// handle worker as a module, which is required for monaco-editor workers to work
 	worker: {
